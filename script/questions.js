3de8a4752c9a91c2b2115c82bc5af46689941c32
Sillas cavalcanti <sillascavalcanti1@gmail.com>
2024-06-16 17:26:11 -0300

    conexao das perguntas

---
+++ script/questions.js
@@ -0,0 +1,63 @@
+function nextQuestionHTML0(){
+    window.location.href = "questionshtml0.html";
+}
+function nextQuestionHTML1(){
+    window.location.href = "questionshtml1.html";
+}
+function nextQuestionHTML2(){
+    window.location.href = "questionshtml2.html";
+}
+function nextQuestionHTML3(){
+    window.location.href = "questionshtml3.html";
+}
+function nextQuestionHTML4(){
+    window.location.href = "questionshtml4.html";
+}
+
+function nextQuestionCSS0(){
+    window.location.href = "questionscss0.html"
+}
+function nextQuestionCSS1(){
+    window.location.href = "questionscss1.html"
+}
+function nextQuestionCSS2(){
+    window.location.href = "questionscss2.html"
+}
+function nextQuestionCSS3(){
+    window.location.href = "questionscss3.html"
+}
+function nextQuestionCSS4(){
+    window.location.href = "questionscss4.html"
+}
+
+function nextQuestionPython0(){
+    window.location.href = "questionspy0.html"
+}
+function nextQuestionPython1(){
+    window.location.href = "questionspy1.html"
+}
+function nextQuestionPython2(){
+    window.location.href = "questionspy2.html"
+}
+function nextQuestionPython3(){
+    window.location.href = "questionspy3.html"
+}
+function nextQuestionPython4(){
+    window.location.href = "questionspy4.html"
+}
+
+function nextQuestionJava0(){
+    window.location.href = "questionsjava0.html"
+}
+function nextQuestionJava1(){
+    window.location.href = "questionsjava1.html"
+}
+function nextQuestionJava2(){
+    window.location.href = "questionsjava2.html"
+}
+function nextQuestionJava3(){
+    window.location.href = "questionsjava3.html"
+}
+function nextQuestionJava4(){
+    window.location.href = "questionsjava4.html"
+}
